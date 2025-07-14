612737508b4ecf5715b97bcc8f4ec45ab58321a4
simjimin <simjimin@simjiminui-MacBookAir.local>
2025-07-14 11:05:19 +0900

    commondblib추가

--- backend/FullAuthSystem/Services/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-namespace FullAuthSystem.Services
-{
-    public interface IEmailService
-    {
-        Task<bool> SendPasswordResetEmailAsync(string email, string verificationCode);
-        Task<bool> SendEmailAsync(string to, string subject, string body);
-    }
-} 
